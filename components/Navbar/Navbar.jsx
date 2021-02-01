0fa725b30c1720071d39d093b6cc7d709cf7de78
Beatrix Woo <beatrixwho@gmail.com>
2021-02-01 12:07:14 -0800

    landing page created

---
+++ components/Navbar/Navbar.jsx
@@ -0,0 +1,8 @@
+export default function Navbar() {
+    return (
+        <div className="flex h-32 w-full p-10 justify-between sticky top-0">
+                <p className="text-white text-4xl">Rate My Solution</p>
+                <button className="bg-white font-bold rounded-full py-3 px-6"><p className="text-primary">Sign In</p></button>
+        </div>
+    )
+}
